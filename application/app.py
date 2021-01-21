68125ad80b27fba26320c99be4513e58402b7806
alailsonko <aalailson3@gmail.com>
2021-01-21 13:18:28 -0300

    feat: impments mgrations and db connection

--- application/app.py
+++ application/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
 
+
 def create_app(config_name):
     app = Flask(__name__)
-    
+
     config_module = f"application.config.{config_name.capitalize()}Config"
-    
+
     app.config.from_object(config_module)
-    
+
+    from application.models import db, migrate
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     @app.route("/")
     def hello_world():
         return "hello, world"
-    
+
     return app
--- application/config.py
+++ application/config.py
@@ -7,23 +7,25 @@ class Config(object):
     """Base configuration"""
     user = os.environ["POSTGRES_USER"]
     password = os.environ["POSTGRES_PASSWORD"]
-    hostname = os.environ["POSTGRES_HOSTNAME"]
+    hostname = os.environ["POSTGRES_HOST"]
     port = os.environ["POSTGRES_PORT"]
     database = os.environ["APPLICATION_DB"]
-    
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql+psycopg2://{user}:{password}@{hostname}:{port}/{database}",
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
+
 class ProductionConfig(Config):
     """Production configuration"""
+
 
 class DevelopmentConfig(Config):
     """Development configuration"""
 
+
 class TestingConfig(Config):
     """Testing configuration"""
-    
+
     TESTING = True
