e203375829073a4396a6adf4ea52f49b94c48d9d
alailsonko <aalailson3@gmail.com>
2021-02-03 18:51:31 -0300

    feat: implements use case addacount

---
+++ application/usecases/add_account.py
@@ -0,0 +1,6 @@
+class AddAccount:
+    def __init__(self, username, email, password, passwordConfirm):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.passwordConfirm = passwordConfirm
