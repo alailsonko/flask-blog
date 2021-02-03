3944f53fdbda42ab875994bbd177eeb849cc75d7
alailsonko <aalailson3@gmail.com>
2021-02-03 18:50:11 -0300

    chore: clean

--- application/create_app/app.py
+++ application/create_app/app.py
@@ -4,7 +4,6 @@ from flask import Flask
 def create_app(config_name):
 
     app = Flask(__name__)
-
     config_module = f"application.config.{config_name.capitalize()}Config"
     app.config.from_object(config_module)
 
