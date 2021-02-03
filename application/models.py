e4f3438c1bb726d352175d6575502d4263687cfd
alailsonko <aalailson3@gmail.com>
2021-02-03 18:49:12 -0300

    feat: update models

--- application/models.py
+++ application/models.py
@@ -8,4 +8,6 @@ migrate = Migrate()
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
